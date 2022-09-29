362116f54695b6f78e09eed8cbff0f28e3a69b66
Joe Lee <j1yl@pm.me>
2022-09-29 05:42:43 -0700

    Change slogan

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className='hero-wrapper'>
       <div className='one'>I AM <span className='name'>JOE LEE</span></div>
-      <div className='two'><span className='adjective'>IMAGINATIVE </span>DEVELOPER</div>
+      <div className='two'><span className='adjective'>WEB </span>DEVELOPER</div>
     </div>
   )
 }
