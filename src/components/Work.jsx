1b902e9e2bb98363caf4d5ec3c41fcce93c09cf0
Joe Lee <j1yl@pm.me>
2022-09-29 10:33:18 -0700

    Add cards

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Work = () => {
   return (
-    <div className='work-container'>Work</div>
+    <div className='work-container'>
+      <div className='card-layout'>
+        <div className='card-layout-items'>
+          <h2>Project #1</h2>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis, metus id vulputate sagittis, dolor augue consequat mi, in rhoncus sem nunc vitae eros. Cras a sollicitudin nibh. In imperdiet aliquam neque. Phasellus in urna blandit ligula dictum facilisis quis vitae augue. Donec tempor ligula sit amet enim congue laoreet. Morbi tempor, velit in accumsan dictum, augue diam tristique nibh, ut rutrum elit libero vel lorem. Ut consequat nisl venenatis diam tempus, in lobortis nulla vulputate. Quisque iaculis, dolor a scelerisque rutrum, ante neque lacinia ex, eget tincidunt ipsum odio sed odio.
+        </div>
+        <div className='card-layout-items'>
+          <h2>Project #2</h2>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis, metus id vulputate sagittis, dolor augue consequat mi, in rhoncus sem nunc vitae eros. Cras a sollicitudin nibh. In imperdiet aliquam neque. Phasellus in urna blandit ligula dictum facilisis quis vitae augue. Donec tempor ligula sit amet enim congue laoreet. Morbi tempor, velit in accumsan dictum, augue diam tristique nibh, ut rutrum elit libero vel lorem. Ut consequat nisl venenatis diam tempus, in lobortis nulla vulputate. Quisque iaculis, dolor a scelerisque rutrum, ante neque lacinia ex, eget tincidunt ipsum odio sed odio.
+        </div>
+        <div className='card-layout-items'>
+          <h2>Project #3</h2>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis, metus id vulputate sagittis, dolor augue consequat mi, in rhoncus sem nunc vitae eros. Cras a sollicitudin nibh. In imperdiet aliquam neque. Phasellus in urna blandit ligula dictum facilisis quis vitae augue. Donec tempor ligula sit amet enim congue laoreet. Morbi tempor, velit in accumsan dictum, augue diam tristique nibh, ut rutrum elit libero vel lorem. Ut consequat nisl venenatis diam tempus, in lobortis nulla vulputate. Quisque iaculis, dolor a scelerisque rutrum, ante neque lacinia ex, eget tincidunt ipsum odio sed odio.
+        </div>
+      </div>
+    </div>
   )
 }
 
