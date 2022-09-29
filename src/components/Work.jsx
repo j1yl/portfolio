33880fd242f3cbd12bf193d4046afa8cf90fa448
Joe Lee <j1yl@pm.me>
2022-09-29 11:09:21 -0700

    Cards

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,17 +4,33 @@ const Work = () => {
   return (
     <div className='work-container'>
       <div className='card-layout'>
-        <div className='card-layout-items'>
-          <h2>Project #1</h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis, metus id vulputate sagittis, dolor augue consequat mi, in rhoncus sem nunc vitae eros. Cras a sollicitudin nibh. In imperdiet aliquam neque. Phasellus in urna blandit ligula dictum facilisis quis vitae augue. Donec tempor ligula sit amet enim congue laoreet. Morbi tempor, velit in accumsan dictum, augue diam tristique nibh, ut rutrum elit libero vel lorem. Ut consequat nisl venenatis diam tempus, in lobortis nulla vulputate. Quisque iaculis, dolor a scelerisque rutrum, ante neque lacinia ex, eget tincidunt ipsum odio sed odio.
+        <div className='card-layout-items' id='about'>
+          <h2 className='card-layout-items-title'>About Me</h2><br></br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tr</p>
         </div>
         <div className='card-layout-items'>
-          <h2>Project #2</h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis, metus id vulputate sagittis, dolor augue consequat mi, in rhoncus sem nunc vitae eros. Cras a sollicitudin nibh. In imperdiet aliquam neque. Phasellus in urna blandit ligula dictum facilisis quis vitae augue. Donec tempor ligula sit amet enim congue laoreet. Morbi tempor, velit in accumsan dictum, augue diam tristique nibh, ut rutrum elit libero vel lorem. Ut consequat nisl venenatis diam tempus, in lobortis nulla vulputate. Quisque iaculis, dolor a scelerisque rutrum, ante neque lacinia ex, eget tincidunt ipsum odio sed odio.
+          <h2 className='card-layout-items-title'>#1</h2><br></br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
         </div>
         <div className='card-layout-items'>
-          <h2>Project #3</h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis, metus id vulputate sagittis, dolor augue consequat mi, in rhoncus sem nunc vitae eros. Cras a sollicitudin nibh. In imperdiet aliquam neque. Phasellus in urna blandit ligula dictum facilisis quis vitae augue. Donec tempor ligula sit amet enim congue laoreet. Morbi tempor, velit in accumsan dictum, augue diam tristique nibh, ut rutrum elit libero vel lorem. Ut consequat nisl venenatis diam tempus, in lobortis nulla vulputate. Quisque iaculis, dolor a scelerisque rutrum, ante neque lacinia ex, eget tincidunt ipsum odio sed odio.
+          <h2 className='card-layout-items-title'>#2</h2><br></br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        </div>
+        <div className='card-layout-items'>
+          <h2 className='card-layout-items-title'>#3</h2><br></br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        </div>
+        <div className='card-layout-items'>
+          <h2 className='card-layout-items-title'>#4</h2><br></br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        </div>
+        <div className='card-layout-items'>
+          <h2 className='card-layout-items-title'>#5</h2><br></br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        </div>
+        <div className='card-layout-items'>
+          <h2 className='card-layout-items-title'>#6</h2><br></br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
         </div>
       </div>
     </div>
