ff894a73dfad7c2c728dfde4d8c3db6b57e29b87
Joe Lee <lefodev@gmail.com>
2022-09-29 23:12:44 -0700

    Revert work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,28 +3,7 @@ import React from 'react'
 const Work = () => {
   return (
     <div className='work-container'>
-      <div className='card-layout'>
-        <div className='card-layout-items' id='about'>
-          <h2 className='card-layout-items-title'>About Me</h2><br></br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tr</p>
-        </div>
-        <div className='card-layout-items'>
-          <h2 className='card-layout-items-title'>#1</h2><br></br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        </div>
-        <div className='card-layout-items'>
-          <h2 className='card-layout-items-title'>#2</h2><br></br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        </div>
-        <div className='card-layout-items'>
-          <h2 className='card-layout-items-title'>#3</h2><br></br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        </div>
-        <div className='card-layout-items'>
-          <h2 className='card-layout-items-title'>#4</h2><br></br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex nec leo euismod ornare. Nullam congue leo id augue ornare, quis vulputate neque elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        </div>
-      </div>
+      Work
     </div>
   )
 }
