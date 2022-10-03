1e3a010e4791fb0195f61e868db61ab256e37d4d
Joe Lee <lefodev@gmail.com>
2022-10-02 20:25:40 -0700

    Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import './App.scss'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Work from './components/Work'
-import Skills from './components/Skills'
-import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <div className='page-wrapper'>
-        <Header/>
+      <div className='page-container'>
+        {/* <Header/> */}
         <Hero/>
-        <Work/>
+        <Projects/>
         <Skills/>
-        <Footer/>
+        <Contact/>
+        {/* <Footer/> */}
       </div>
     </div>
   )
--- src/components/Skills.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Skills = () => {
-  return (
-    <div className='skills-container'>Skills</div>
-  )
-}
-
-export default Skills
