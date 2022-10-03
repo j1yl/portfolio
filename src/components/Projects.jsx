5de9bc33d733f66404656fa1571f0001e6b33bcb
Joe Lee <lefodev@gmail.com>
2022-10-02 21:37:36 -0700

    Add some css

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Projects = () => {
   return (
     <section className='two'>
-      <h2>HERE ARE SOME OF MY PROJECTS</h2>
+      <h2>MY WORK</h2>
       <div className='project-container'>
         <div className='card'>
           <div className='card__cover'>
-            CARD COVER HERE
+            Image
           </div>
           <div className='card__content'>
             <h3>Project Name</h3>
@@ -16,7 +16,7 @@ const Projects = () => {
         </div>
         <div className='card'>
           <div className='card__cover'>
-            CARD COVER HERE
+            Image
           </div>
           <div className='card__content'>
             <h3>Project Name</h3>
@@ -25,7 +25,7 @@ const Projects = () => {
         </div>
         <div className='card'>
           <div className='card__cover'>
-            CARD COVER HERE
+            Image
           </div>
           <div className='card__content'>
             <h3>Project Name</h3>
@@ -34,16 +34,7 @@ const Projects = () => {
         </div>
         <div className='card'>
           <div className='card__cover'>
-            CARD COVER HERE
-          </div>
-          <div className='card__content'>
-            <h3>Project Name</h3>
-            <p>Project Description Blah blahb lahblh albhalhb lahblah lbhlabhahlbal hblbah bhlahb ahl bhabhl ahbabl abhl</p>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card__cover'>
-            CARD COVER HERE
+            Image
           </div>
           <div className='card__content'>
             <h3>Project Name</h3>
