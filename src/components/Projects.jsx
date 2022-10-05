15d7766b46952480ffebbbad041e56d5bd71e987
Joe Lee <lefodev@gmail.com>
2022-10-04 22:14:49 -0700

    Added project component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <>
+      <section className="project-container">
+        <h3>Project 1</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et
+          laoreet mi, eu ultricies quam. Pellentesque pellentesque elementum
+          lobortis. Donec scelerisque ipsum ac arcu mollis efficitur. Donec
+          vestibulum vel massa sit amet facilisis. Proin eros ipsum, placerat in
+          condimentum ornare, ullamcorper sit amet lorem. In vitae mi nisi. Sed
+          maximus lectus in metus semper, mattis aliquet lectus euismod. Quisque
+          lobortis lobortis augue, ut facilisis mauris condimentum vestibulum.
+        </p>
+        <a href="/">Github</a>
+        <a href="/">Deployment</a>
+      </section>
+      <section className="project-container">
+        <h3>Project 1</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et
+          laoreet mi, eu ultricies quam. Pellentesque pellentesque elementum
+          lobortis. Donec scelerisque ipsum ac arcu mollis efficitur. Donec
+          vestibulum vel massa sit amet facilisis. Proin eros ipsum, placerat in
+          condimentum ornare, ullamcorper sit amet lorem. In vitae mi nisi. Sed
+          maximus lectus in metus semper, mattis aliquet lectus euismod. Quisque
+          lobortis lobortis augue, ut facilisis mauris condimentum vestibulum.
+        </p>
+        <a href="/">Github</a>
+        <a href="/">Deployment</a>
+      </section>
+    </>
+  );
+};
+
+export default Projects;
