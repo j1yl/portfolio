1b9dfa8f2088dd52282f8f736ba431e270cb65ec
Joe Lee <j1yl@pm.me>
2022-10-25 10:12:51 -0700

    Skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="App font-poppins">
       <Header />
       <Hero />
+      <Skills />
     </div>
   );
 }
