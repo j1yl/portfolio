00a707670e0f3276e760317754a965450a4aa59a
Joe Lee <lefodev@gmail.com>
2022-10-27 16:26:23 -0700

    Added cursor div and animate based on cursor position

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Works from "./components/Works";
 import Contact from "./components/Contact";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 function App() {
   return (
@@ -13,7 +15,48 @@ function App() {
       <Skills />
       <Works />
       <Contact />
+      <Cursor />
     </div>
+  );
+}
+
+function Cursor() {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    const mouseMove = (e: any) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  });
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 32,
+      y: mousePosition.y - 32,
+    },
+    click: {
+      x: mousePosition.x - 32,
+      y: mousePosition.y - 32,
+      scale: 0.9,
+    },
+  };
+
+  return (
+    <motion.div
+      variants={variants}
+      animate="default"
+      className="h-16 w-16 rounded-full fixed top-0 left-0 z-100 pointer-events-none border-2 border-zinc-900"
+    />
   );
 }
 
