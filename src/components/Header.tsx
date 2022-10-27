292ae27732fe8cd3283343909fef9e6a058d6ec8
Joe Lee <lefodev@gmail.com>
2022-10-26 20:41:26 -0700

    Add react-scroll dependency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-      <header className="w-full fixed top-0 left-0 z-10">
+      <header className="w-full fixed top-0 left-0 z-10 bg-zinc-100">
         <nav className="mx-auto max-w-6xl flex flex-row justify-between py-6 px-4 md:px-0 items-center text-zinc-900">
           <a href="/" className="text-xl font-medium">
             LEFO
