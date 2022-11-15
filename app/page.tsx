7bd6f948092aaa06f32d2ee12fd9d89ee4af1bc1
Joe Lee <j1yl@pm.me>
2022-11-15 06:54:40 -0800

    Add some hero text

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <main className="flex flex-col">
+        <span>JOE LEE</span>
+        <span>WEB DEVELOPER</span>
+        <span>
+          I design and developer websites from scratch using the latest
+          technology. I am currently pursuing an undergraduate degree in
+          Computer Science at California State University, Fullerton.
+        </span>
+        <button>CONTACT ME</button>
+        <span>FINDING BEAUTY IN SIMPLICITY</span>
+        <span>
+          I am committed to crafting the best web experiences. I optimize
+          loading times, follow SEO best practices, and develop with precision
+          on every project.
+        </span>
+      </main>
+    </>
+  );
 }
