a4bb44c5db6aea79ada5828e000e0a42e5d41f82
Joe Lee <lefodev@gmail.com>
2022-11-15 17:18:53 -0800

    Add css styles for hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,38 @@
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col">
-        <span>JOE LEE</span>
-        <span>WEB DEVELOPER</span>
-        <span>
-          I design and developer websites from scratch using the latest
-          technology. I am currently pursuing an undergraduate degree in
-          Computer Science at California State University, Fullerton.
-        </span>
-        <button>CONTACT ME</button>
-        <span>FINDING BEAUTY IN SIMPLICITY</span>
-        <span>
-          I am committed to crafting the best web experiences. I optimize
-          loading times, follow SEO best practices, and develop with precision
-          on every project.
-        </span>
+      <main className="flex flex-col h-[90vh] justify-center px-2 md:px-8">
+        <section className="flex flex-col gap-5">
+          <div className="flex flex-col">
+            <h1 className="text-5xl md:text-9xl font-bold leading-none">
+              JOE LEE
+            </h1>
+            <h2 className="text-5xl md:text-9xl font-bold leading-none">
+              WEB DEVELOPER
+            </h2>
+          </div>
+          <p className="md:w-2/3 w-full text-base">
+            I design and developer websites from scratch using the latest
+            technology. I am currently pursuing an undergraduate degree in
+            Computer Science at{" "}
+            <span className="underline">
+              California State University, Fullerton.
+            </span>
+          </p>
+          <button className="w-max h-max text-3xl md:text-[3rem] border border-black rounded-lg px-8 py-3 md:px-16 md:py-6">
+            CONTACT ME
+          </button>
+        </section>
+        <section className="mt-16 flex flex-col gap-5">
+          <h2 className="text-3xl md:text-[3rem] font-bold">
+            FINDING BEAUTY IN SIMPLICITY
+          </h2>
+          <p className="md:w-2/3 w-full text-base">
+            I am committed to crafting the best web experiences. I optimize
+            loading times, follow SEO best practices, and develop with precision
+            on every project.
+          </p>
+        </section>
       </main>
     </>
   );
