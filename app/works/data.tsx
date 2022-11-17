c74ab2b2b5ce38f774ea04523a8215ecf383d486
Joe Lee <j1yl@pm.me>
2022-11-17 10:38:49 -0800

    Add data.tsx for project data

---
+++ app/works/data.tsx
@@ -0,0 +1,59 @@
+const proj = {
+  1: {
+    name: "LEFODEV",
+    description:
+      "An animated web application made using industry standard practices to showcase my information and past projects.",
+    tags: ["REACT", "NEXTJS", "TAILWINDCSS", "TYPESCRIPT", "FRAMER"],
+    github: "https://github.com/j1yl/lefo",
+    preview: "https://lefo.dev",
+  },
+  2: {
+    name: "SHRINKURL",
+    description:
+      "A simple to use web application that shortens a given link using the gotiny.cc API.",
+    tags: ["TYPESCRIPT", "TAILWINDCSS", "REACT", "VITE"],
+    github: "https://github.com/j1yl/shrinkurl",
+    preview: "https://shrinkurl.vercel.app",
+  },
+  3: {
+    name: "IASSEMBLE ANYTHING",
+    description:
+      "A responsive web application for the company iAssemble Anything, developed and designed from scratch.",
+    tags: ["REACT", "NEXTJS", "TAILWINDCSS", "TYPESCRIPT", "FRAMER", "EMAILJS"],
+    github: "https://github.com/j1yl/iassemble",
+    preview: "https://iassemble.vercel.app",
+  },
+  4: {
+    name: "FAIRVIEW VENTURES",
+    description:
+      "A responsive web application for the company Fairview Ventures, developed and designed from scratch.",
+    tags: ["REACT", "NEXTJS", "TAILWINDCSS", "TYPESCRIPT", "FRAMER", "EMAILJS"],
+    github: "https://github.com/j1yl/fairview",
+    preview: "https://fairview.vercel.app",
+  },
+  5: {
+    name: "HEAVENSENT",
+    description:
+      "A recreation of a design for a product's landing page. Custom 3D model was imported and used in the hero.",
+    tags: [
+      "TYPESCRIPT",
+      "REACT",
+      "TAILWINDCSS",
+      "VITE",
+      "REACT-THREE/FIBER",
+      "REACT-THREE/DREI",
+    ],
+    github: "https://github.com/j1yl/product",
+    preview: "https://heavensent.vercel.app",
+  },
+  6: {
+    name: "SPOTYLYZER",
+    description:
+      "A web application with authentication that retrieves the user's Spotify data. Data includes top played tracks and artists. Due to restrictions set by Spotify, this program cannot be made public. A demo will be provided on GitHub.",
+    tags: ["JAVASCRIPT", "REACT", "NEXT", "SASS", "SPOTIFYAPI", "NEXTAUTH"],
+    github: "https://github.com/j1yl/spotlyzer",
+    preview: "https://i.imgur.com/MCMZu1y.png",
+  },
+};
+
+export default proj;
