85dfaf68b5c8360df3d285add30c7be927d66a06
Joe Lee <lefodev@gmail.com>
2022-11-17 15:32:38 -0800

    Add gif links

--- app/works/data.js
+++ app/works/data.js
@@ -1,37 +1,41 @@
-const proj = {
-  1: {
+const proj = [
+  {
     name: "LEFODEV",
     description:
       "An animated web application made using industry standard practices to showcase my information and past projects.",
     tags: ["REACT", "NEXTJS", "TAILWINDCSS", "TYPESCRIPT", "FRAMER"],
     github: "https://github.com/j1yl/lefo",
     preview: "https://lefo.dev",
+    image: "https://i.imgur.com/tzIOXg7.gif",
   },
-  2: {
+  {
     name: "SHRINKURL",
     description:
       "A simple to use web application that shortens a given link using the gotiny.cc API.",
     tags: ["TYPESCRIPT", "TAILWINDCSS", "REACT", "VITE"],
     github: "https://github.com/j1yl/shrinkurl",
     preview: "https://shrinkurl.vercel.app",
+    image: "https://i.imgur.com/zhL7NrT.gif",
   },
-  3: {
+  {
     name: "IASSEMBLE ANYTHING",
     description:
       "A responsive web application for the company iAssemble Anything, developed and designed from scratch.",
     tags: ["REACT", "NEXTJS", "TAILWINDCSS", "TYPESCRIPT", "FRAMER", "EMAILJS"],
     github: "https://github.com/j1yl/iassemble",
     preview: "https://iassemble.vercel.app",
+    image: "https://i.imgur.com/zhL7NrT.gif",
   },
-  4: {
+  {
     name: "FAIRVIEW VENTURES",
     description:
       "A responsive web application for the company Fairview Ventures, developed and designed from scratch.",
     tags: ["REACT", "NEXTJS", "TAILWINDCSS", "TYPESCRIPT", "FRAMER", "EMAILJS"],
     github: "https://github.com/j1yl/fairview",
     preview: "https://fairview.vercel.app",
+    image: "https://i.imgur.com/zhL7NrT.gif",
   },
-  5: {
+  {
     name: "HEAVENSENT",
     description:
       "A recreation of a design for a product's landing page. Custom 3D model was imported and used in the hero.",
@@ -45,15 +49,17 @@ const proj = {
     ],
     github: "https://github.com/j1yl/product",
     preview: "https://heavensent.vercel.app",
+    image: "https://i.imgur.com/v4GssXr.png",
   },
-  6: {
+  {
     name: "SPOTYLYZER",
     description:
       "A web application with authentication that retrieves the user's Spotify data. Data includes top played tracks and artists. Due to restrictions set by Spotify, this program cannot be made public. A demo will be provided on GitHub.",
     tags: ["JAVASCRIPT", "REACT", "NEXT", "SASS", "SPOTIFYAPI", "NEXTAUTH"],
     github: "https://github.com/j1yl/spotlyzer",
     preview: "https://i.imgur.com/MCMZu1y.png",
+    image: "https://i.imgur.com/MCMZu1y.png",
   },
-};
+];
 
 export default proj;
