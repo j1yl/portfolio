80a373c71f46a4b8b3b5dcf7f048866fd8740678
Joe Lee <lefodev@gmail.com>
2022-11-18 15:07:12 -0800

    Add media queries to icon

--- app/head.tsx
+++ app/head.tsx
@@ -7,7 +7,12 @@ export default function Head() {
         name="description"
         content="Designing and developing websites using the latest technology. A portfolio website for Joe Lee, Web Developer"
       />
-      <link rel="icon" href="/lefo.svg" />
+      <link rel="icon" href="/lefo.svg" media="(prefers-color-scheme: dark)" />
+      <link
+        rel="icon"
+        href="/lefolight.svg"
+        media="(prefers-color-scheme: light)"
+      />
     </>
   );
 }
