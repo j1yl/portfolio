b3c412a66f0b8203364094f56c11aa185bea4543
Joe Lee <lefodev@gmail.com>
2022-11-22 23:38:41 -0800

    Floating word component

--- components/FloatingWord.tsx
+++ components/FloatingWord.tsx
@@ -2,28 +2,42 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const FloatingWord = ({ text }: { text: string }) => {
+const FloatingWord = ({ text, delay }: { text: string; delay: number }) => {
   let charArr = [];
-  charArr = text.split("");
-  console.log(charArr);
-  //{letter === " " ? "\u00A0" : letter}
+  charArr = text.split(" ");
+
+  const container = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.1, delayChildren: 0.12 * i + delay },
+    }),
+  };
+
+  const child = {
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      y: 30,
+    },
+  };
+
   return (
     <>
-      <motion.div>
+      <motion.div
+        variants={container}
+        initial="hidden"
+        whileInView="visible"
+        className="overflow-hidden flex flex-wrap select-none"
+      >
         {charArr.map((item, index) => (
-          <motion.span
-            key={index}
-            whileHover={{
-              style: {
-                color: "#ffffff",
-              },
-              transition: {
-                duration: 0.2,
-              },
-            }}
-            onHoverStart={(e) => {}}
-            onHoverEnd={(e) => {}}
-          >
+          <motion.span key={index} variants={child} className="relative mr-2">
             {item}
           </motion.span>
         ))}
@@ -31,5 +45,5 @@ const FloatingWord = ({ text }: { text: string }) => {
     </>
   );
 };
-
+FloatingWord;
 export default FloatingWord;
