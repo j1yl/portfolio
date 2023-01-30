068c3aaacc76e1a23d1477e3c2b764c51e3fc870
Joe Lee <lefodev@gmail.com>
2023-01-30 13:28:55 -0800

    Add navbar and footer to layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 };
 
 export default MyApp;
