3833db75245fcea9cc1d14255157fcaf414681aa
Joe Lee <lefodev@gmail.com>
2023-01-30 22:41:50 -0800

    Add basic navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex items-center justify-between px-4 py-2">
+      <Link href="/" className="px-4 py-2 text-2xl font-bold">
+        lefo.dev
+      </Link>
+      <div className="flex w-full justify-end text-sm">
+        <Link href="/" className="link">
+          ABOUT
+        </Link>
+        <Link href="/" className="link">
+          WORKS
+        </Link>
+        <Link href="/" className="link">
+          CONTACT
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
