3ac85625a2832a118f6a3fe7dd8b885f7325c536
Joe Lee <j1yl@pm.me>
2023-01-31 18:40:39 -0800

    Change font to Dosis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import { EB_Garamond } from "@next/font/google";
+import { Dosis } from "@next/font/google";
 import "../styles/globals.css";
 
-const eb = EB_Garamond({ subsets: ["latin"] });
+const dosis = Dosis({ subsets: ["latin"] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
-      <div className={eb.className}>
+      <div className={dosis.className}>
         <Navbar />
         <Component {...pageProps} />
         <Footer />
