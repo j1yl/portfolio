90bf0137937048c1770377c97b142c786ffeab60
Joe Lee <111937630+j1yl@users.noreply.github.com>
2023-01-31 19:29:45 -0800

    Add config for maps (#5)
    * Add partial config
    
    * Add most of config
    
    ---------
    
    Co-authored-by: Joe Lee <j1yl@pm.me>

--- public/config.ts
+++ public/config.ts
@@ -1,48 +1,73 @@
-export const skills = ["", "", "", "", ""];
+export const languages = ["TypeScript", "Python", "C++", "HTML", "CSS"];
+
+export const frameworks = [
+  "React",
+  "NextJS",
+  "SvelteKit",
+  "Supabase",
+  "Prisma",
+  "Node",
+  "Express",
+  "MongoDB",
+];
 
 export const projects = [
   {
-    id: "",
-    title: "",
-    previewLink: "",
-    githubLink: "",
-    techstack: ["", "", "", "", ""],
+    id: "1",
+    title: "SaveArt",
+    previewLink: "saveart.vercel.app",
+    githubLink: "https://github.com/j1yl/saveart",
+    techstack: ["Sveltekit", "Prisma", "Supabase", "TailwindCSS"],
+    status: "wip",
   },
   {
-    id: "",
-    title: "",
-    previewLink: "",
-    githubLink: "",
-    techstack: ["", "", "", "", ""],
+    id: "2",
+    title: "ShrinkURL",
+    previewLink: "shrinkurl.vercel.app",
+    githubLink: "https://github.com/j1yl/shrinkurl",
+    techstack: ["TypeScript", "React", "TailwindCSS", "GotinyAPI"],
+    status: "finished",
   },
   {
-    id: "",
-    title: "",
+    id: "3",
+    title: "Spotlyzer",
     previewLink: "",
-    githubLink: "",
-    techstack: ["", "", "", "", ""],
+    githubLink: "https://github.com/j1yl/spotlyzer",
+    techstack: ["JavaScript", "SASS", "React", "NextJS", "SpotifyAPI"],
+    status: "finished",
   },
   {
-    id: "",
-    title: "",
+    id: "4",
+    title: "EasyCommerce",
     previewLink: "",
-    githubLink: "",
-    techstack: ["", "", "", "", ""],
+    githubLink: "https://github.com/j1yl/easycommerce",
+    techstack: ["TypeScript", "NextJS", "React", "Sanity", "Stripe"],
+    status: "wip",
   },
   {
-    id: "",
-    title: "",
+    id: "5",
+    title: "Cmail",
     previewLink: "",
-    githubLink: "",
-    techstack: ["", "", "", "", ""],
+    githubLink: "https://github.com/j1yl/cmail",
+    techstack: ["Python", "Yagmail", "Pandas"],
+    status: "finished",
   },
   {
-    id: "",
-    title: "",
-    previewLink: "",
-    githubLink: "",
-    techstack: ["", "", "", "", ""],
+    id: "6",
+    title: "Portfolio",
+    previewLink: "https://lefo.dev",
+    githubLink: "https://github.com/j1yl/lefo",
+    techstack: [
+      "TypeScript",
+      "NextJS",
+      "React",
+      "TailwindCSS",
+      "Framer Motion",
+      "EmailJS",
+    ],
+    status: "finished",
   },
 ];
 
-export const resume = "";
+export const resume =
+  "https://drive.google.com/file/d/1bXIGnkkfMF7bSC-93_IiKM_si2lbdjrz/view?usp=share_link";
