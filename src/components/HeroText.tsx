0335c473eae0399ef290c74f4690b7f01d95a37b
Joe Lee <j1yl@pm.me>
2023-02-07 18:54:47 -0800

    Add HeroText component

---
+++ src/components/HeroText.tsx
@@ -0,0 +1,69 @@
+import { motion } from "framer-motion";
+
+const myColors: string[] = ["#9fc0ff", "#c89dff", "#ffcc9d", "#ff9d9d"];
+
+const parent = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const child = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 100,
+      bounce: 2,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: -20,
+  },
+};
+
+const HeroText = ({ text }: { text: string }) => {
+  const chars = text.split("");
+  const randomColor = (array: string[]) => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+  return (
+    <motion.div
+      className="flex"
+      variants={parent}
+      initial="hidden"
+      animate="visible"
+    >
+      {chars.map((letter, index) => (
+        <motion.span
+          onHoverStart={() => {}}
+          onHoverEnd={() => {}}
+          whileHover={{
+            scaleY: [1, 1, 0.9, 1],
+            scaleX: [1, 1.3, 0.9, 1],
+            color: randomColor(myColors),
+          }}
+          transition={{
+            duration: 0.3,
+            type: "spring",
+            stiffness: 100,
+            bounce: 2,
+          }}
+          variants={child}
+          key={index}
+          className="h-max w-max bg-clip-text text-5xl font-bold"
+        >
+          {letter === " " ? "\u00A0" : letter}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
+};
+
+export default HeroText;
