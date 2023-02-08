5ebbbb2794e12911dbf022f42a72713f813d11c0
Joe Lee <lefodev@gmail.com>
2023-02-07 19:43:40 -0800

    Add some logic for mouse positioning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,63 @@
-import { type NextPage } from "next";
-import Head from "next/head";
-import Hero from "../components/Hero";
 import { languages, frameworks, projects } from "../../public/config";
 import Link from "next/link";
 import Image from "next/image";
+import { type NextPage } from "next";
+import Head from "next/head";
 import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { env } from "../env/client.mjs";
+import useMouse from "@react-hook/mouse-position";
+import { motion } from "framer-motion";
+import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
+  const ref = useRef(null);
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  const mouse = useMouse(ref, {
+    enterDelay: 100,
+    leaveDelay: 100,
+  });
+
+  let mouseXPosition: number = 0;
+  let mouseYPosition: number = 0;
+
+  if (mouse.x !== null) {
+    mouseXPosition = mouse.clientX as number;
+  }
+
+  if (mouse.y !== null) {
+    mouseYPosition = mouse.clientY as number;
+  }
+
+  const circleCSS =
+    "hidden z-100 pointers-events-none font-xl fixed top-0 left-0 md:flex flex-row items-center justify-center rounded-full bg-black text-center";
+
+  const variants = {
+    default: {
+      opacity: 1,
+      height: 32,
+      width: 32,
+      x: mouseXPosition - 16,
+      y: mouseYPosition - 16,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+      },
+    },
+    herotext: {
+      opacity: 1,
+      height: 64,
+      width: 64,
+      x: mouseXPosition - 32,
+      y: mouseYPosition - 32,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+      },
+    },
+  };
+
   return (
     <>
       <Head>
@@ -19,12 +68,24 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center p-4 font-mont">
-        <Hero />
-      </main>
-      <About />
-      <Works />
-      <Contact />
+      <div ref={ref}>
+        <motion.div
+          className={circleCSS}
+          variants={variants}
+          animate={cursorVariant}
+          transition={{
+            type: "spring",
+            damping: 50,
+            stiffness: 500,
+          }}
+        ></motion.div>
+        <main className="flex min-h-screen flex-col items-center justify-center p-4 font-mont">
+          <Hero />
+        </main>
+        <About />
+        <Works />
+        <Contact />
+      </div>
     </>
   );
 };
