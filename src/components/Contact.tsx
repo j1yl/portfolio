90efd5e49e9e917d9e0242b2f106fd694e33964b
Joe Lee <lefodev@gmail.com>
2023-02-08 21:04:13 -0800

    Add more styles

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,8 @@
 import { useState, useRef } from "react";
 import { env } from "../env/client.mjs";
 import emailjs from "@emailjs/browser";
+import { motion } from "framer-motion";
+import HeroText from "./HeroText";
 
 const Contact = () => {
   const [formSubmit, setFormSubmit] = useState(false);
@@ -8,7 +10,6 @@ const Contact = () => {
 
   const handleFormSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    setFormSubmit(true);
     emailjs
       .sendForm(
         env.NEXT_PUBLIC_SERVICEID,
@@ -24,58 +25,75 @@ const Contact = () => {
           console.error(error);
         }
       );
-
+    setFormSubmit(true);
     form.current?.reset();
   };
 
   return (
-    <section className="fullpage min-w-6xl mx-auto max-w-6xl p-4" id="contact">
+    <section className="fullpage p-4">
+      <div className="min-w-6xl mx-auto flex w-full max-w-6xl p-4">
+        <HeroText text={"Send me a message!"} />
+      </div>
       <form
         ref={form}
-        className="flex w-full flex-col gap-4"
         onSubmit={handleFormSubmit}
+        className="min-w-6xl flex h-full w-full max-w-6xl flex-col gap-4 p-4 text-primary_dark dark:text-primary_white"
       >
-        <div className="flex flex-col gap-2">
-          <label className="font-semibold">Name</label>
-          <input
-            type="text"
-            className="rounded-xl bg-[#FF9D9D] p-2 placeholder-[#121212] focus:outline-none"
-            placeholder="Enter your name"
-            name="name"
-          />
+        <div className="flex flex-col justify-between gap-4 md:flex-row">
+          <InputField name="name" type="text" text="What is your name?" />
+          <InputField name="email" type="email" text="What is your email?" />
         </div>
-        <div className="flex flex-col gap-2">
-          <label className="font-semibold">Email</label>
-          <input
-            type="text"
-            className="rounded-xl bg-[#C89DFF] p-2 placeholder-[#121212] focus:outline-none"
-            placeholder="Enter your email"
-            name="email"
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label className="font-semibold">Message</label>
+        <div>
+          <label className="font-semibold">What is your message?</label>
           <textarea
-            className="resize-none rounded-xl bg-[#FFCC9D] p-2 placeholder-[#121212] focus:outline-none"
-            placeholder="Enter your message"
             rows={5}
             maxLength={1000}
-            name="message"
+            className="w-full bg-third_white p-4 focus:outline-none dark:bg-primary_dark dark:placeholder-primary_white"
           />
         </div>
-        <div className="flex items-center gap-6">
-          <button
-            type="submit"
-            className="w-max rounded-xl bg-[#9FC0FF] px-4 py-2 font-semibold"
-          >
+        <div className="flex justify-between gap-4">
+          <button className="w-max bg-primary_dark px-4 py-2 font-semibold text-primary_white transition-all active:scale-95 dark:bg-primary_dark">
             Submit
           </button>
-          {formSubmit && (
-            <p className="font-semibold">Your message has been sent !</p>
+          {formSubmit ? (
+            <motion.p
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              className="font-semibold"
+            >
+              Your message has been sent !
+            </motion.p>
+          ) : (
+            <></>
           )}
         </div>
       </form>
     </section>
+  );
+};
+
+const InputField = ({
+  text,
+  name,
+  type,
+}: {
+  text: string;
+  name: string;
+  type: string;
+}) => {
+  const [data, setData] = useState<HTMLInputElement>();
+
+  return (
+    <>
+      <div className="flex w-full flex-col gap-2">
+        <label className="font-semibold">{text}</label>
+        <input
+          type={type}
+          name={name}
+          className="bg-third_white p-4 focus:outline-none dark:bg-primary_dark dark:placeholder-primary_white"
+        />
+      </div>
+    </>
   );
 };
 
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -2,15 +2,16 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { projects } from "../../public/config";
+import HeroText from "./HeroText";
 
 const variants = {
   animate: {
-    x: [2000, -2000],
+    x: [0, -200],
     transition: {
       x: {
         repeat: Infinity,
         repeatType: "loop",
-        duration: 25,
+        duration: 10,
         ease: "linear",
       },
     },
@@ -19,7 +20,51 @@ const variants = {
 
 const Works = () => {
   return (
-    <section className="fullpage w-full max-w-6xl p-2" id="works"></section>
+    <section
+      className="fullpage mx-auto w-full max-w-6xl p-4 font-semibold  text-primary_dark dark:text-primary_white"
+      id="works"
+    >
+      <div className="flex w-full flex-col gap-4">
+        <HeroText text={"My Projects"} />
+        <div className="relative flex w-full max-w-6xl flex-col gap-2 overflow-x-hidden overflow-y-clip">
+          {projects.map((proj) => (
+            <Marquee
+              text={proj.title.toUpperCase()}
+              id={proj.id}
+              key={proj.id}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Marquee = ({ text, id }: { text: string; id: string }) => {
+  return (
+    <motion.div
+      variants={variants}
+      whileHover="animate"
+      className="static whitespace-nowrap text-4xl dark:text-third_white md:text-8xl"
+    >
+      <Link href={`/works/${id}`}>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+        <span>{text}</span>
+      </Link>
+    </motion.div>
   );
 };
 
