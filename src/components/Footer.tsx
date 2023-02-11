64c93b05801de2fd9c3280a8771a346f5ebaef48
Joe Lee <111937630+j1yl@users.noreply.github.com>
2023-02-10 23:17:07 -0800

    Add footer (#20)
    * Add footer
    
    * Fix navbar svg
    
    ---------
    
    Co-authored-by: Joe Lee <j1yl@pm.me>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="min-w-6xl mx-auto flex w-full max-w-6xl flex-row justify-end p-4 text-sm">
+      <div className="flex gap-4 text-primary_dark dark:text-primary_white">
+        <Link href="https://github.com/j1yl">GitHub</Link>
+        <Link href="https://linkedin.com/in/j1yl">LinkedIn</Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
