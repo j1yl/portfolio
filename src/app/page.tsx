921ea8f8eaaf4de5c686db891615eec0b73740c0
Joe Lee <lefodev@gmail.com>
2023-05-07 14:46:08 -0700

    Add glitch vfx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
+"use client";
+
 import Bubble from "@/components/ui/Bubble";
 import Socials from "@/components/ui/Socials";
 import { socials } from "@/server/content";
+import { useState } from "react";
 
 export default function Home() {
+  const [glitch, setGlitch] = useState(true);
+
   return (
     <main className="flex my-16 md:my-48 flex-col items-center justify-between p-4 leading-loose tracking-wide">
       <div className="flex flex-col h-full max-w-4xl gap-8">
-        <h1 className="h1">Unlocking the Power of the Web</h1>
+        {glitch ? (
+          <h1 className="h1 glitch">
+            <span aria-hidden={true}>Unlocking the Power of the Web</span>
+            Unlocking the Power of the Web
+            <span aria-hidden={true}>Unlocking the Power of the Web</span>
+          </h1>
+        ) : (
+          <h1 className="h1">Unlocking the Power of the Web</h1>
+        )}
         <p className="p">
           Hi there! My name is Joe Lee, a web developer with experience in
           building responsive, user-friendly websites. Whether you&apos;re
@@ -25,10 +38,25 @@ export default function Home() {
           <Bubble text="Contact" url="/contact" />
           <Bubble text="lefodev@gmail.com" url="mailto:lefodev@gmail.com" />
         </div>
-        <div className="flex w-full gap-4">
-          {socials.map((s) => (
-            <Socials platform={s.platform} url={s.url} key={s.id} />
-          ))}
+        <div className="flex flex-col-reverse md:flex-row gap-8 justify-between">
+          <div className="flex w-full gap-4">
+            {socials.map((s) => (
+              <Socials platform={s.platform} url={s.url} key={s.id} />
+            ))}
+          </div>
+          <div className="flex w-full md:justify-end justify-start">
+            <label className="relative inline-flex items-center mb-4 cursor-pointer">
+              <input
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+                onChange={() => setGlitch(!glitch)}
+                checked={glitch}
+              />
+              <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-gray-50 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              <span className="ml-3 p">Glitch Effect</span>
+            </label>
+          </div>
         </div>
       </div>
     </main>
